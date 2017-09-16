6b4fdf43bf2c7467184e4e1aad02b7af51579349
yantze <yantze@126.com>
2017-09-16 11:06:12 +0800

    Initial Commit

---
+++ mapmine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mapmine
+//
+//  Created by zhi on 9/16/17.
+//  Copyright © 2017 zhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
